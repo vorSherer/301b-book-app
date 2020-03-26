eadf205f21e1b0127d2481a50ec0607edc5e9fd3
vorSherer <tcpma2011@gmail.com>
2020-03-25 19:17:29 -0700

    Got CSS working again (some), Books save to database and display from it, and added details.ejs page.

--- seed.sql
+++ seed.sql
@@ -1,3 +1,6 @@
--- CONTENT COMING - Ran out of time
 
--- INSERT INTO savedbooks ...
+INSERT INTO savedbooks (title, authors, thumbnail_url, description) VALUES ('ninja turtles', 'Donatello', 'http://goesNowhere.com', 'Four turtles and a rat walk into a bar...');
+
+INSERT INTO savedbooks (title, authors, thumbnail_url, description) VALUES ('star dreck', 'Kirkus Maximus', 'http://goesWhereNoManHasGoneBefore.com', 'Four command staff and a red shirt beam into a bar, with Scotty...');
+
+SELECT * FROM savedbooks;
